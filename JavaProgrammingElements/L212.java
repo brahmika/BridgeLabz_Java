9148f5f6c74977d151c975ae913f575cf6623fc7
brahmika <brahmika30@gmail.com>
2026-01-28 17:14:16 +0530

    [Brahmika]: All the Java Programming Element assignment is done.

---
+++ JavaProgrammingElements/L212.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class L212 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // User input for weight in pounds
+        System.out.print("Enter weight in pounds: ");
+        double weightInPounds = sc.nextDouble();
+
+        // Conversion: 1 pound = 2.2 kg
+        double weightInKg = weightInPounds / 2.2;
+
+        System.out.println("The weight of the person in pound is " + weightInPounds +
+                           " and in kg is " + weightInKg);
+    }
+}

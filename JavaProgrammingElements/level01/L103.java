c96e91e039c3ba474ec3516a4d525f72ef82d7eb
Brahmika Shree Balaji <59389153+brahmika@users.noreply.github.com>
2026-01-28 16:42:59 +0530

    Add files via upload

---
+++ JavaProgrammingElements/level01/L103.java
@@ -0,0 +1,9 @@
+public class L103 {
+    public static void main(String[] args){
+        double distance = 10.8;
+        double miles = distance * 1.6;
+
+        System.out.println("The distance"+ distance + "km in miles is" + miles);
+        
+    }
+}

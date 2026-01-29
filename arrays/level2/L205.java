6e298271625d109be842363187ee2ddc8fa38d07
brahmika <brahmika30@gmail.com>
2026-01-29 14:28:24 +0530

    [Brahmika]:Added arryas  exercises in level1, level2

---
+++ arrays/level2/L205.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class L205 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int tempNumber = number;
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (tempNumber != 0) {
+            if (index == maxDigit) {
+                maxDigit += 10;
+                int[] temp = new int[maxDigit];
+                for (int i = 0; i < digits.length; i++) {
+                    temp[i] = digits[i];
+                }
+                digits = temp;
+            }
+
+            digits[index] = tempNumber % 10;
+            tempNumber /= 10;
+            index++;
+        }
+
+        System.out.println("Reversed number digits:");
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+        System.out.println();
+    }
+}

15c30fe5f98ae6102b4d5a3ebbe3cb64ef8b8f10
brahmika <brahmika30@gmail.com>
2026-01-29 14:28:24 +0530

    [Brahmika]:Added arryas  exercises in level1, level2

---
+++ arrays/level1/L102.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class L102 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is positive and even");
+                } else {
+                    System.out.println(num + " is positive and odd");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is negative");
+            } else {
+                System.out.println(num + " is zero");
+            }
+        }
+
+        int first = numbers[0];
+        int last = numbers[numbers.length - 1];
+
+        if (first == last) {
+            System.out.println("First and last elements are equal");
+        } else if (first > last) {
+            System.out.println("First element is greater than last element");
+        } else {
+            System.out.println("First element is less than last element");
+        }
+    }
+}

0f994a14456b1a9deeffd0c2d7e0a9ab5576b25b
brahmika <brahmika30@gmail.com>
2026-01-29 14:11:19 +0530

    Add ControlFlow exercises in level1, level2, level3

---
+++ ControlFlows/level1/L1018.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class L112 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("The number is not a natural number");
+            return;
+        }
+
+        // Sum using while loop
+        int sumWhile = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sumWhile += i;
+            i++;
+        }
+
+        // Sum using formula
+        int sumFormula = n * (n + 1) / 2;
+
+        System.out.println("Sum using while loop: " + sumWhile);
+        System.out.println("Sum using formula: " + sumFormula);
+
+        if (sumWhile == sumFormula) {
+            System.out.println("Both results are correct and equal");
+        } else {
+            System.out.println("The results are not equal");
+        }
+    }
+}
---
+++ ControlFlows/level3/L306.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class L302 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+
+       
+        int count = 0;
+        int temp = number; 
+
+
+        if (temp == 0) {
+            count = 1; 
+        } else {
+            while (temp != 0) {
+                temp = temp / 10; 
+                count++;           
+            }
+        }
+
+        System.out.println("The number of digits in " + number + " is " + count);
+    }
+}

3d426f0b441aa5fa8d38bf80bbe4d2344fa7fe5b
Brahmika Shree Balaji <59389153+brahmika@users.noreply.github.com>
2026-02-02 09:23:38 +0530

    [Brahmika]: To check if Divisible By Five
    Refactored main method for better clarity and added comments.

--- ControlFlows/level1/DivisibleByFive.java
+++ ControlFlows/level1/DivisibleByFive.java
@@ -1,14 +1,31 @@
-// Write a program to check if a number is divisible by 5
+// Import Scanner class to take input from the user
 import java.util.Scanner;
+
+// Class to check if a number is divisible by 5
 public class DivisibleByFive {
-    public static void main(String[] args){
-        try (Scanner input = new Scanner(System.in)) {
-            int num = input.nextInt();
-            if (num % 5 == 0) {
-                System.out.println(num + " is divisible by 5");
-            } else{
-                System.out.println(num + " is not divisible by 5");
-            }
+
+    // Main method where program execution starts
+    public static void main(String[] args) {
+
+        // Create Scanner object to read input from keyboard
+        Scanner input = new Scanner(System.in);
+
+        // Read an integer value from the user
+        int num = input.nextInt();
+
+        // Check if the number is divisible by 5
+        if (num % 5 == 0) {
+
+            // Print message if number is divisible by 5
+            System.out.println(num + " is divisible by 5");
+
+        } else {
+
+            // Print message if number is not divisible by 5
+            System.out.println(num + " is not divisible by 5");
         }
+
+        // Close the Scanner object
+        input.close();
     }
 }
